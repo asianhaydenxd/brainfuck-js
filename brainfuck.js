5cbc963029ab393a3442a92732808a345fcfabf6
Hayden <asianhaydenxd@gmail.com>
2022-07-16 00:10:21 -0400

    Implement print feature

--- brainfuck.js
+++ brainfuck.js
@@ -2,14 +2,23 @@ function interpret(code) {
     let index = 0;
 
     let pointer = 0;
-    let tape = [0];
+    const tape = [0];
 
-    while (index < code.length()) {
+    while (index < code.length) {
         switch (code[index]) {
-            case '+':
+            case '.': // Output pointed cell as per its ASCII value
+                console.log(tape[pointer]); // This is temporary (just prints the number value) until I figure out how to convert ints into ASCII chars
+                break;
+            case '+': // Increment pointed cell by 1
                 tape[pointer] += 1;
+                break;
+            case '-': // Decrement pointed cell by 1
+                tape[pointer] -= 1;
+                break;
         }
+
+        index += 1;
     }
 }
 
-interpret("+");
+interpret("++++.");
