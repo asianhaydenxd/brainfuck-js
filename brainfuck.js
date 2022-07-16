a13518c009d5d76b6115d4f61403f731ba02ff50
Hayden <asianhaydenxd@gmail.com>
2022-07-16 02:05:49 -0400

    Implement an input backlog

--- brainfuck.js
+++ brainfuck.js
@@ -23,20 +23,34 @@ async function interpret(code) {
     const tape = [0];
     const braceMap = makeBraceMap(code);
 
+    let inputBacklog = "";
+
     while (index < code.length) {
         switch (code[index]) {
             case '.': // Output pointed cell as per its ASCII value
                 process.stdout.write(String.fromCharCode(tape[pointer])); // This is temporary (just prints the number value) until I figure out how to convert ints into ASCII chars
                 break;
             case ',':
+                if (inputBacklog.length > 0) {
+                    tape[pointer] = inputBacklog.charCodeAt(0);
+                    inputBacklog = inputBacklog.slice(1)
+                    break;
+                }
+
                 const readline = require('readline');
                 const rl = readline.createInterface({ input: process.stdin, output: process.stdout, terminal: false });
                 const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
-                const input = await prompt('')
+
+                let input = await prompt('');
+
+                rl.close();
+                rl.on('close', () => process.exit(0));
+                
+                if (input.length == 0) throw "Input must be exactly 1 character!";
+
+                if (input.length > 1) inputBacklog += input.slice(1);
+
                 tape[pointer] = input.charCodeAt(0);
-                rl.close()
-                rl.on('close', () => process.exit(0))
-                if (input.length != 1) throw "Input must be exactly 1 character!"
                 break;
             case '+': // Increment pointed cell by 1
                 tape[pointer] += 1;
