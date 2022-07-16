d127e133ba0fc4766bf5ceef768e8a30951e21e4
Hayden <asianhaydenxd@gmail.com>
2022-07-16 00:21:10 -0400

    Actually print out the ASCII

--- brainfuck.js
+++ brainfuck.js
@@ -7,7 +7,7 @@ function interpret(code) {
     while (index < code.length) {
         switch (code[index]) {
             case '.': // Output pointed cell as per its ASCII value
-                console.log(tape[pointer]); // This is temporary (just prints the number value) until I figure out how to convert ints into ASCII chars
+                process.stdout.write(String.fromCharCode(tape[pointer])); // This is temporary (just prints the number value) until I figure out how to convert ints into ASCII chars
                 break;
             case '+': // Increment pointed cell by 1
                 tape[pointer] += 1;
@@ -31,4 +31,4 @@ function interpret(code) {
     }
 }
 
-interpret("-.");
+interpret(".+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+.+");
